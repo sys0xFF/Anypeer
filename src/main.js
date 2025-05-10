5c91a7b487ea5c99f6dfe8daec53fc7a6cefe0c1
Anthony <anthony.sforzin@gmail.com>
2025-05-10 05:10:32 -0300

    feat: setup main Electron window

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const { create } = require('domain');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.loadFile(path.join(__dirname, '../public/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
